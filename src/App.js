98705d46a8333ed920ae1a5245d40aba07e21efb
Ali <ali@alighali.com>
2018-04-28 00:38:22 +0200

    feature(nav-bar): add nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import NavBar from './components/navBar/NavBar.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">React works!</div>
+      <div className="App">
+        <NavBar/>
+      </div>
     );
   }
 }
---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid , NavItem, NavDropdown, Nav, MenuItem, Navbar } from 'react-bootstrap';
+
+const NavBar = () => <Navbar>
+<Navbar.Header>
+  <Navbar.Brand>
+    <a href="#home">البصيرة</a>
+  </Navbar.Brand>
+</Navbar.Header>
+</Navbar>;
+
+export default NavBar;
