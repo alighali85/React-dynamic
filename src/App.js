1f46a904540f6fea7c339ddbfdab333513c66338
Ali Ghali <ali@alighali.com>
2018-04-29 00:11:14 +0200

    Revert "Feature/customized theme"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
-import AppNavBar from './components/appNavBar/AppNavBar.js';
-import CarouselSlider from './components/carouselSlider/CarouselSlider.js';
-import SocialMediaBar from './components/socialMediaBar/SocialMediaBar.js';
-import CallToActionBar from './components/callToActionBar/CallToActionBar.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-        <CarouselSlider />
-        <CallToActionBar />
-        <SocialMediaBar />
-      </div>
+      <div className="App">React works!</div>
     );
   }
 }
