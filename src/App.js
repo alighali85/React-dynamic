77746d5da7f6904c80de5ecf9cd089a924789ba3
Ali <ali@alighali.com>
2018-05-02 00:09:17 +0200

    feature(app-body): add cards to jumbtron

--- src/App.js
+++ src/App.js
@@ -6,6 +6,42 @@ import CallToActionBar from './components/callToActionBar/CallToActionBar.js'
 import NavTabs from './components/navTabs/NavTabs'
 import AppFooter from './components/appFooter/AppFooter'
 import AppJumbtron from './components/appJumbotron/AppJumbotron.js'
+import CardImage from './assest/img/card-header-1.jpg'
+
+const Youtube = () => (
+  <iframe width='100%' height='315' src='https://www.youtube.com/embed/tgbNymZ7vqY' />
+)
+
+const Articles = () => (
+  <div>
+    <div className='card col-md-3'>
+      <img className='card-img-top' src={CardImage} alt='Card cap' />
+      <div className='card-body'>
+        <p className='card-text'>يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء </p>
+      </div>
+    </div>
+    <div className='card col-md-3'>
+      <img className='card-img-top' src={CardImage} alt='Card cap' />
+      <div className='card-body'>
+        <p className='card-text'>يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء </p>
+      </div>
+    </div>
+    <div className='card col-md-3'>
+      <img className='card-img-top' src={CardImage} alt='Card cap' />
+      <div className='card-body'>
+        <p className='card-text'>يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء </p>
+      </div>
+    </div>
+    <div className='card col-md-3'>
+      <img className='card-img-top' src={CardImage} alt='Card cap' />
+      <div className='card-body'>
+        <p className='card-text'>يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء </p>
+      </div>
+      <br />
+      <hr />
+    </div>
+  </div>
+)
 
 class App extends Component {
   render () {
@@ -15,8 +51,8 @@ class App extends Component {
         <CarouselSlider />
         <CallToActionBar />
         <SocialMediaBar />
-        <AppJumbtron />
-        <AppJumbtron />
+        <AppJumbtron children={<Articles />} />
+        <AppJumbtron children={<Youtube />} />
         <AppJumbtron />
         <AppFooter />
       </div>
