70e01331a2edc151f1f0d92116cdae8ee5d867da
Ali <ali@alighali.com>
2018-05-01 07:07:28 +0200

    feature(app-footer): adjustthe footer to be responsive

--- src/assest/constants/AppMainContent.js
+++ src/assest/constants/AppMainContent.js
@@ -1,14 +1,16 @@
-export const APP_NAME = 'Al-Bassera';
-export const APP_NAME_ARABIC = 'البصيرة';
+export const APP_NAME = 'Al-Bassera'
+export const APP_NAME_ARABIC = 'البصيرة'
 
-export const APP_CONTACT_EMAIL = 'Al-Bassera';
-export const APP_CONTACT_PHONE = '021 444 543 099';
+export const APP_CONTACT_EMAIL = 'Al-Bassera'
+export const APP_CONTACT_PHONE = '021 444 543 099'
 
-export const APP_CTA_CALL_US = 'اتصل بنا الآن';
-export const APP_CTA_SOCILA_MEDIA = 'تواصل معنا عبر ';
-export const APP_CTA_WHATSAPP = 'واتس آب';
+export const APP_CTA_CALL_US = 'اتصل بنا الآن'
+export const APP_CTA_SOCILA_MEDIA = 'تواصل معنا عبر '
+export const APP_CTA_WHATSAPP = 'واتس آب'
 
-export const APP_CTA_YOUTUBE = 'يوتيوب';
-export const APP_CTA_FACEBOOK = 'فيس بوك';
-export const APP_CTA_INSTAGRAM = 'انستاغرام';
-export const APP_CTA_TWITTER = 'اتويتر';
+export const APP_CTA_YOUTUBE = 'يوتيوب'
+export const APP_CTA_FACEBOOK = 'فيس بوك'
+export const APP_CTA_INSTAGRAM = 'انستاغرام'
+export const APP_CTA_TWITTER = 'اتويتر'
+
+export const COPYRIGHT = 'تطبيق البصيرة لصاحبة السيد ٢٠١٨ جميع الحقوق محفوظة'
