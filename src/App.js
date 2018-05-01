595662e4b7ff1364657f00278c276706dc03c21a
Ali <ali@alighali.com>
2018-05-01 22:56:35 +0200

    feature(app-body): add app-jumbtron

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
-import AppNavBar from './components/appNavBar/AppNavBar.js';
-import CarouselSlider from './components/carouselSlider/CarouselSlider.js';
-import SocialMediaBar from './components/socialMediaBar/SocialMediaBar.js';
-import CallToActionBar from './components/callToActionBar/CallToActionBar.js';
-import NavTabs from './components/navTabs/NavTabs';
+import React, { Component } from 'react'
+import AppNavBar from './components/appNavBar/AppNavBar.js'
+import CarouselSlider from './components/carouselSlider/CarouselSlider.js'
+import SocialMediaBar from './components/socialMediaBar/SocialMediaBar.js'
+import CallToActionBar from './components/callToActionBar/CallToActionBar.js'
+import NavTabs from './components/navTabs/NavTabs'
 import AppFooter from './components/appFooter/AppFooter'
+import AppJumbtron from './components/appJumbotron/AppJumbotron.js'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <AppNavBar children={ <NavTabs /> }/>
+      <div className='App'>
+        <AppNavBar children={<NavTabs />} />
         <CarouselSlider />
         <CallToActionBar />
         <SocialMediaBar />
+        <AppJumbtron />
         <AppFooter />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/appJumbotron/AppJumbotron.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react'
+import { Jumbotron, Button, Grid } from 'react-bootstrap'
+import './app-jumbotron.scss'
+
+class componentName extends Component {
+  render () {
+    return (
+      <Grid>
+        <Jumbotron>
+          <h2>مرحبا أيها العالم!</h2>
+          <p>
+          هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق. إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء .
+          </p>
+          <p>
+            <Button bsStyle='primary'>المزيد</Button>
+          </p>
+        </Jumbotron>;
+      </Grid>
+
+    )
+  }
+}
+
+export default componentName
