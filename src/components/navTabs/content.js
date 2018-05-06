22ed4c814826de1b9d105bbffae7b90a001bc670
Ali <ali@alighali.com>
2018-05-06 23:57:59 +0200

    feature(navbar): make the content comes form object file

---
+++ src/components/navTabs/content.js
@@ -0,0 +1,92 @@
+export const navTabsContent = [
+  {
+    title: 'جلب الحبيب',
+    key: 1,
+    link: 'www.google.com',
+    content: [
+      {
+        title: 'tab contant one 1',
+        image: 'http://react.rocks/images/converted/react-google-maps.jpg',
+        link: 'link one ',
+        key: 1
+      },
+      {
+        title: 'tab contant one 2',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      },
+      {
+        title: 'tab contant one 3',
+        image: 'https://www.apkmirror.com/wp-content/uploads/2016/11/582388b75080d-384x384.png',
+        link: ''
+
+      }
+    ]
+  },
+  {
+    title: 'علاج العقم',
+    key: 2,
+    content: [
+      {
+        title: 'tab contant two 1',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+
+      },
+      {
+        title: 'tab contant two 2',
+        image: 'https://www.apkmirror.com/wp-content/uploads/2016/11/582388b75080d-384x384.png',
+        link: '',
+        key: 4
+      },
+      {
+        title: 'tab contant two 3',
+        image: 'https://www.apkmirror.com/wp-content/uploads/2016/11/582388b75080d-384x384.png',
+        link: ''
+
+      }
+    ]
+  },
+  {
+    title: 'خلطات الزوجية',
+    key: 3,
+    content: [
+      {
+        title: 'tab contant three 1',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      },
+      {
+        title: 'tab contant three 2',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      },
+      {
+        title: 'tab contant three 3',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      }
+    ]
+  },
+  {
+    title: 'أحجار روحانية',
+    key: 4,
+    content: [
+      {
+        title: 'tab contant one',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      },
+      {
+        title: 'tab contant one',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      },
+      {
+        title: 'tab contant one',
+        image: 'https://static1.squarespace.com/static/559ca9fce4b0015f541296e6/t/594c9a2a17bffc24e8c00906/1498192434043/',
+        link: ''
+      }
+    ]
+  }
+]
