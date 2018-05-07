2531365263d5fe426dfba2e9fa5eb68669b721b8
Ali <ali@alighali.com>
2018-05-07 23:03:53 +0200

    feature(nav-side-bar): style the slidenav

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './app-theme/index.scss'
-import { BREAK_POINTS } from './assest/constants/BreakPoints'
 
 ReactDOM.render(
   <App />,
