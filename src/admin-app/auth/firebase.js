4e2c881abaac5fd3592f267ddf3138a1910dc21a
Ali <ali@alighali.com>
2018-05-20 01:45:11 +0200

    refactor(app): firebase file

--- src/admin-app/auth/firebase.js
+++ src/admin-app/auth/firebase.js
@@ -1,24 +1,12 @@
-import firebase from 'firebase'
+import * as firebase from 'firebase'
 
 const dtabaseRefernces = {
   categories: 'Categories'
 }
 
-const adminAppdatabaseConfig = {
-  apiKey: 'AIzaSyC7_ZlYpn-_T1RnCfSfTj_uoVpdm54Chtc',
-  authDomain: 'albassera-44a86.firebaseapp.com',
-  databaseURL: 'https://albassera-44a86.firebaseio.com',
-  projectId: 'albassera-44a86',
-  storageBucket: 'albassera-44a86.appspot.com',
-  messagingSenderId: '729501309659'
-}
-
-firebase.initializeApp(adminAppdatabaseConfig)
-
-export const adminAppdatabase = firebase.database()
-export const categoriesData = adminAppdatabase.ref().child(dtabaseRefernces.categories)
-
-export function getCategories () {
+export function getDatafromServer () {
+  const adminAppdatabase = firebase.database()
+  const categoriesData = adminAppdatabase.ref().child(dtabaseRefernces.categories)
   categoriesData.on('value', snap => console.log(snap.val()))
   console.log('Automatic function fired')
 }
