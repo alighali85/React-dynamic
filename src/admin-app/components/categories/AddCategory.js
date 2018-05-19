cdbcd416da6828ee8f694c0c4dcc28d785040bb8
Ali <ali@alighali.com>
2018-05-20 01:46:42 +0200

    feat(app): create category components

---
+++ src/admin-app/components/categories/AddCategory.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { Form, Col, FormGroup, FormControl, Button } from 'react-bootstrap'
+import * as firebase from 'firebase'
+
+class AddCategory extends Component {
+
+  constructor(props) { 
+    super(props)
+    this.state= {
+      categoryName: '',
+      categoryTitle: '',
+      showCategory: '',
+      allowSend: false
+    }
+  }
+
+  handleSubmit = (e) => {
+    const {  categoryName, categoryTitle, showCategory } = this.state
+    e.preventDefault()
+    const adminAppdatabase = firebase.database()
+    const categoriesData = adminAppdatabase.ref().child('Categories')
+    categoriesData.push({
+      categoryName: categoryName,
+      categoryTitle: categoryTitle,
+      showCategory: showCategory
+    }, function(error) {
+      if (error) {
+        console.log('// The write failed...')
+      } else {
+        console.log('// Data saved successfully!')
+      //TO DO:  Do something when the request comes back .... 
+    } 
+  })
+  console.log('will be redirect to categories')
+  setTimeout(() => {
+    this.props.onFormSent()
+  }, 1000);
+  
+  }
+
+  handleInput = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+  render () {
+
+    
+    return (
+      <div >
+        <hr />
+        <Form horizontal onSubmit={this.handleSubmit}>
+          <FormGroup controlId='formHorizontalEmail'>
+            <Col sm={10}>
+              <FormControl type='text' name='categoryName' placeholder='ادهل اسم للقسم' onChange={this.handleInput}/>
+            </Col>
+            <Col sm={2}>
+            اسم القسم            </Col>
+          </FormGroup>
+
+          <FormGroup controlId='formHorizontalEmail'>
+            <Col sm={10}>
+              <FormControl name='categoryTitle' type='text' placeholder='' onChange={this.handleInput}/>
+            </Col>
+            <Col sm={2}>عنوان العرض </Col>
+          </FormGroup>
+
+          <FormGroup controlId='formControlsSelect'>
+            <Col sm={10}>
+              <FormControl name='showCategory' componentClass='select' placeholder='select' onChange={this.handleInput}>
+                <option value='0'>عرض في الأقسام</option>
+                <option value='1'>عدم العرض</option>
+              </FormControl>
+            </Col>
+            <Col sm={2}>خيارات العرض </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col smOffset={6} sm={6}>
+              <Button type='submit'>أضافة قسم</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default AddCategory
+
--- src/admin-app/components/categories/Categories.js
+++ src/admin-app/components/categories/Categories.js
@@ -1,26 +1,160 @@
 import React, { Component } from 'react'
 import './categories.scss'
 import { ListGroup, ListGroupItem } from 'react-bootstrap'
+import * as firebase from 'firebase'
+import DeleteIcon from '../../assets/images/delete-button.svg'
+import EditIcon from '../../assets/images/edit.svg'
+import PagesIcon from '../../assets/images/pages.svg'
+import ListDots from '../../assets/images/list-dots.svg'
+import AddCategory from './AddCategory'
+import EditCategory from './EditCategory'
+import ConfirmWindow from '../confirmWindow/ConfirmWindow'
+import TitleWithIcon from '../elements/TitleWithIcon'
+import ButtonWithIcon from '../elements/ButtonWithIcon';
 
 class Categories extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      cat: [],
+      showConfirm: false,
+      addCategoryMode: false,
+      editCategoryMode: false,
+      holdCatId: null,
+      categoryToEdit: null
+    }
+  }
+
+  // Control Delet Category 
+  handleDeleterequest = (e) => {
+    const { name } = e.target
+    this.setState({
+      showConfirm: true,
+        holdCatId: name
+    })
+  }
+
+  handleDeleteItem = () => {
+    const { holdCatId } = this.state
+    firebase.database().ref('Categories').child(holdCatId).remove()
+    this.setState({
+      showConfirm: false,
+      holdCatId: null
+    })
+  }
+
+  dismissConfirmation = () => {
+    this.setState({
+      showConfirm: false,
+      holdCatId: null
+    })
+  }
+
+  // Control Add Category 
+  addCategoryMode = () => {
+    this.setState({
+      addCategoryMode: true
+    })
+  }
+
+  ExitAddCategoryMode = () => {
+    this.setState({
+      addCategoryMode: false
+    })
+  }
+
+  // control Edit Category 
+  editCategoryMode = (e) => {
+    const { name } = e.target
+    this.setState({
+      editCategoryMode: true,
+      categoryToEdit: name
+    })
+  }
+
+  ExitEditCategoryMode = () => {
+    this.setState({
+      editCategoryMode: false
+    })
+  }
+
+  componentWillMount () {
+    const adminAppdatabase = firebase.database()
+    const categoriesData = adminAppdatabase.ref().child('Categories')
+    categoriesData.on('value', (snap) => {
+      var categories = []
+      snap.forEach((cat) => {
+        categories.push({
+          key: cat.key,
+          ...cat.val()
+        })
+      })
+      this.setState({
+        cat: categories
+      })
+    })
+  }
+
   render () {
+    const { cat, showConfirm, addCategoryMode, editCategoryMode, categoryToEdit } = this.state
     return (
       <div className='admin-categories'>
-        Admin Categories page
-        <CategoriesList />
+
+        <TitleWithIcon
+          title='إدارة الأقسام'
+          icon='warehouse'
+        />
+
+        <ButtonWithIcon
+          onClick={this.addCategoryMode}
+          text='إضافة قسم'
+          iconName='magic'
+          ButtonStyle='success'
+          float='left'
+        />
+
+        {cat.map((item, i) => <ListGroup>
+          <ListGroupItem header={item.categoryName - i} >
+            <img className='list-user-icon' src={DeleteIcon} alt='delete' 
+            name={item.key} 
+            onClick={(e)=> this.handleDeleterequest(e)} />
+
+            <img className='list-user-icon' src={EditIcon} alt='edit'
+            name={item.key}
+            onClick={this.editCategoryMode}/>
+
+            <img className='list-user-icon' src={PagesIcon} alt='Pages' />
+            <img className='list-icon' src={ListDots} alt='dots' />{item.categoryName} </ListGroupItem>
+          </ListGroup>
+          )}
+
+        <ConfirmWindow confirmation
+          show={showConfirm}
+          onConfirm={this.handleDeleteItem}
+          onDismiss={this.dismissConfirmation}
+          title='حذف قسم رئيسي'
+          text='حذف قسم رئيسي.. لايمكنك الإستعادة بعد الحذف'
+        />
+            
+        <ConfirmWindow 
+          show={addCategoryMode}
+          onDismiss={this.ExitAddCategoryMode}
+          title='إضافة قسم رئيسي'
+          text='جميع الحقول مطلوبة '>
+          <AddCategory onFormSent={this.ExitAddCategoryMode} />
+        </ConfirmWindow>
+        
+        <ConfirmWindow 
+          show={editCategoryMode}
+          onDismiss={this.ExitEditCategoryMode}
+          title='تعديل قسم رئيسي'
+          text='جميع الحقول مطلوبة '>
+          <EditCategory Categorykey={categoryToEdit} onFormSent={this.ExitAddCategoryMode} />
+        </ConfirmWindow>
+
       </div>
     )
   }
 }
-
-const CategoriesList = () => <ListGroup>
-  <ListGroupItem header='Heading 1'>Some body text</ListGroupItem>
-  <ListGroupItem header='Heading 2' href='#'>
-  Linked item
-  </ListGroupItem>
-  <ListGroupItem header='Heading 3' bsStyle='danger'>
-  Danger styling
-  </ListGroupItem>
-</ListGroup>
 
 export default Categories
