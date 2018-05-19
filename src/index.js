2a7ddd9b58cdb770f4719ddd24921927d261238b
Ali <ali@alighali.com>
2018-05-20 01:40:51 +0200

    feat(app): initialize fire base in index file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import App from './App'
 import AdminApp from './admin-app/index.js'
 import './app-theme/index.scss'
 import { BrowserRouter, Route } from 'react-router-dom'
+import * as firebase from 'firebase'
+
+const adminAppdatabaseConfig = {
+  apiKey: 'AIzaSyC7_ZlYpn-_T1RnCfSfTj_uoVpdm54Chtc',
+  authDomain: 'albassera-44a86.firebaseapp.com',
+  databaseURL: 'https://albassera-44a86.firebaseio.com',
+  projectId: 'albassera-44a86',
+  storageBucket: 'albassera-44a86.appspot.com',
+  messagingSenderId: '729501309659'
+}
+
+firebase.initializeApp(adminAppdatabaseConfig)
 
 ReactDOM.render(
   <BrowserRouter>
