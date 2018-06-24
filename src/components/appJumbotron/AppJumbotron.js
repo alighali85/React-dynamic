83cf4432a315b2b4d0c349b1d123a724dbc2e973
Ali <ali@alighali.com>
2018-06-24 16:27:01 +0200

    fix(frontpage): fix jubotron ietms for mobiles
    * fix padding
    * fix ersponsive

--- src/components/appJumbotron/AppJumbotron.js
+++ src/components/appJumbotron/AppJumbotron.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react'
-import { Jumbotron, Button, Grid } from 'react-bootstrap'
+import { Jumbotron, Grid } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './app-jumbotron.scss'
 import PropTypes from 'prop-types'
 
@@ -23,7 +24,7 @@ class AppJumbotron extends Component {
   render () {
     const { title, content } = this.props
     const featuredItem = content.slice(content.length - 1, content.length)
-    content.pop()
+    const relatedItems = content.slice(0, content.length - 1)
     return (
       <Grid className='app-jumbotron'>
         <Jumbotron>
@@ -32,19 +33,29 @@ class AppJumbotron extends Component {
             {featuredItem.map(item => <div className='app-jumbotron__featured'>
               <h3 className='app-jumbotron__featured-name'>{item.name}</h3>
               <img className='app-jumbotron__featured-image' src={item.image} alt={item.name} />
-              <p className='app-jumbotron__featured-title'>{item.title} ...
-                <a className='app-jumbotron__featured-more-link'>المزيد</a>
+              <p className='app-jumbotron__featured-title'>{item.title}
+                <Link
+                  to={`/category/page/${item.pageId}`}
+                  className='app-jumbotron__featured-more-link'>
+                المزيد
+                </Link>
               </p>
             </div>)}
-            {content.map(item => <div className='app-jumbotron__related'>
-              <img className='app-jumbotron__related-image' src={item.image} alt={item.name} />
-              <h3 className='app-jumbotron__related-name'>{item.name}</h3>
-              <p className='app-jumbotron__related-title'>{item.title}</p>
-            </div>)}
-            <hr />            {this.props.children}
-            <p>
-              <Button bsStyle='primary' />
-            </p>
+            <div className='app-jumbotron__related-container'>
+              {relatedItems.map(item => <div className='app-jumbotron__related'>
+                <img className='app-jumbotron__related-image' src={item.image} alt={item.name} />
+                <h3 className='app-jumbotron__related-name'>{item.name}</h3>
+                <p className='app-jumbotron__related-title'>{item.title}
+                  <Link
+                    to={`/category/page/${item.pageId}`}
+                    className='app-jumbotron__featured-more-link'>
+                المزيد
+                  </Link>
+                </p>
+              </div>)}
+            </div>
+            <hr />
+            {this.props.children}
           </div>
         </Jumbotron>
       </Grid>
