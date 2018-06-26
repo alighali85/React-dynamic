0e6b38abdefc64aad1c7d326fb67439f1e85b3ed
Ali <ali@alighali.com>
2018-06-26 19:41:19 +0200

    fix(APP_OCNSTANTS):  change app mainphone number

--- src/assest/constants/AppMainContent.js
+++ src/assest/constants/AppMainContent.js
@@ -2,8 +2,8 @@ export const APP_NAME = 'Al-Bassera'
 export const APP_NAME_ARABIC = 'البصيرة'
 
 export const APP_CONTACT_EMAIL = 'Al-Bassera'
-export const APP_CONTACT_PHONE = '00905362152929'
-export const APP_CONTACT_WHATSAPP = '00905362152929'
+export const APP_CONTACT_PHONE = '00905533347333'
+export const APP_CONTACT_WHATSAPP = '00905533347333'
 
 export const APP_CTA_CALL_US = 'اتصل بنا الآن'
 export const APP_CTA_SOCILA_MEDIA = 'تواصل معنا عبر '
@@ -12,7 +12,7 @@ export const APP_CTA_WHATSAPP = 'واتس آب'
 export const APP_CTA_YOUTUBE = 'يوتيوب'
 export const APP_CTA_FACEBOOK = 'فيس بوك'
 export const APP_CTA_INSTAGRAM = 'انستاغرام'
-export const APP_CTA_TWITTER = 'اتويتر'
+export const APP_CTA_TWITTER = 'تويتر'
 
 export const RELATED_PAGES_NUMBER = 3
 
