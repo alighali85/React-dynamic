8619b0f5974a56b9d3af3e4779936cae5cbf4786
Ali <ali@alighali.com>
2018-07-02 11:31:23 +0200

    feat(side-slider): change the slider content
    * change the slider content
    * add links

--- src/assest/constants/AppMainContent.js
+++ src/assest/constants/AppMainContent.js
@@ -4,6 +4,7 @@ export const APP_NAME_ARABIC = 'البصيرة'
 export const APP_CONTACT_EMAIL = 'Al-Bassera'
 export const APP_CONTACT_PHONE = '00905533347333'
 export const APP_CONTACT_WHATSAPP = '00905533347333'
+export const APP_CONTACT_TWITTER = 'albaserah_7'
 
 export const APP_CTA_CALL_US = 'اتصل بنا الآن'
 export const APP_CTA_SOCILA_MEDIA = 'تواصل معنا عبر '
@@ -19,3 +20,29 @@ export const RELATED_PAGES_NUMBER = 3
 export const NAVBAR_FIXED_TABS_NUMBER = 4
 
 export const COPYRIGHT = 'تطبيق البصيرة لصاحبه الروحاني أبو وليد ٢٠١٨ جميع الحقوق محفوظة'
+
+export const CONTACT_SLIDE = {
+  controls: true,
+  interval: '500',
+  headline: 'اتصل بنا عبر',
+  source: [
+    {
+      title: 'واتس أب',
+      text: APP_CONTACT_PHONE,
+      icon: 'fab fa-whatsapp',
+      link: `https://wa.me/905533347333`
+    },
+    {
+      title: 'الموبايل',
+      text: APP_CONTACT_PHONE,
+      icon: 'fas fa-phone-volume',
+      link: 'https://www.facebook.com/albaserah.web.7'
+    },
+    {
+      title: 'فيس بوك',
+      text: APP_CTA_FACEBOOK,
+      icon: 'fab fa-facebook-square',
+      link: `tel:${APP_CONTACT_PHONE}`
+    }
+  ]
+}
--- src/components/sideSlider/SideSlider.js
+++ src/components/sideSlider/SideSlider.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Panel, Carousel } from 'react-bootstrap'
 import './side-slider.scss'
 import FontAwesome from 'react-fontawesome'
+import { CONTACT_SLIDE } from '../../assest/constants/AppMainContent'
 
 const propTypes = {
   source: PropTypes.array,
@@ -11,33 +12,16 @@ const propTypes = {
   controls: PropTypes.bool
 }
 
-const defaultProps = {
-  controls: true,
-  interval: '500',
-  headline: 'اتصل بنا عبر',
-  source: [
-    {
-      title: 'واتس أب',
-      text: '0966379892',
-      icon: 'whatsapp'
-    },
-    {
-      title: 'الموبايل',
-      text: '0966379892',
-      icon: 'mobile-alt'
-    },
-    {
-      title: 'فيس بوك',
-      text: '/albasserah',
-      icon: 'apple'
-    }
-  ]
-}
+const defaultProps = CONTACT_SLIDE
 
 export default class SideSlider extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
+  }
+
+  goToLink = (link) => {
+    window.location.href = link
   }
 
   render () {
@@ -48,12 +32,14 @@ export default class SideSlider extends React.Component {
           <Carousel indicators={indicators} interval={interval} controls={controls}>
             { source.map((item, i) => <Carousel.Item key={i}>
               <br />
-              <Carousel.Caption>
+              <Carousel.Caption onClick={() => this.goToLink(item.link)}>
                 <h3 className='side-slider__title'>{item.title}</h3>
-                <FontAwesome name={item.icon} size='4x' /><br />
+                <i className={`fab ${item.icon} fa-4x`} />
+                <br />
                 <h4 className='side-slider__text'>{item.text}</h4>
               </Carousel.Caption>
-            </Carousel.Item>)}
+            </Carousel.Item>)
+            }
           </Carousel>
         </Panel>
       </div>
